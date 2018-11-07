2ee8a6f806b3f9d16b360e681514873c503a4437
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2018-11-07 08:06:11 +0330

    get meta tags

--- 01.php
+++ 01.php
@@ -1,4 +1,6 @@
 <?php
     //header('content-type: text/plain');
-    $html = file_get_contents('https://gosafir.com/fa');
-    echo htmlentities($html);
+    //$html = file_get_contents('https://gosafir.com/fa');
+    $tags = get_meta_tags('https://gosafir.com/fa');
+    echo $tags['description']."<br>";
+    echo $tags['keywords']."<br>";
